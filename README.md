95ec11249c79640d2d02e18337b13e1ea0c91924
Emaneliforp <kietchouam@gmail.com>
2022-11-17 22:26:42 -0800

    Finished expose and explore

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Lab 8 - Starter
+Name: Chi Kiet Chou
+1) Within a Github action that runs whenever code is pushed because you want to make sure your commit is working before requesting for a pull request. 
+Besides, you might forget to run test if you chosen it manually and there are room for too many errors if you run the test after the development is completed.
+2) Yes
+3) No, because the whole message feature is too broad to write a sufficient unit test.
+4) Yes, because the condition is well defined and testable.
+
+Screenshots:
+![result-1](./result%20of%20npm%20test%201.PNG)
+![result-2](./result%20of%20npm%20test%202.PNG)
+![result-3](./result%20of%20npm%20test%203.PNG)
+![result-4](./result%20of%20npm%20test%204.PNG)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,44 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('isPhoneNumber', () => {
+    expect(functions.isPhoneNumber("(626)123-1234")).toBe(true);
+    expect(functions.isPhoneNumber("+1(123)123-1234")).toBe(true);
+    expect(functions.isPhoneNumber("6261231234")).toBe(false);
+    expect(functions.isPhoneNumber("abc@gmail.com")).toBe(false);
+});
+
+test('isEmail', () => {
+    expect(functions.isEmail("abc@gmail.com")).toBe(true);
+    expect(functions.isEmail("abc@ucsd.edu")).toBe(true);
+    expect(functions.isEmail("hi@hello")).toBe(false);
+    expect(functions.isEmail("123456789")).toBe(false);
+});
+
+test('isEmail', () => {
+    expect(functions.isEmail("abc@gmail.com")).toBe(true);
+    expect(functions.isEmail("abc@ucsd.edu")).toBe(true);
+    expect(functions.isEmail("hi@hello")).toBe(false);
+    expect(functions.isEmail("123456789")).toBe(false);
+});
+
+test('isStrongPassword', () => {
+    expect(functions.isStrongPassword("abcd1234")).toBe(true);
+    expect(functions.isStrongPassword("Abcd_123_")).toBe(true);
+    expect(functions.isStrongPassword("@BCD1234")).toBe(false);
+    expect(functions.isStrongPassword("123456789")).toBe(false);
+});
+
+test('isDate', () => {
+    expect(functions.isDate("01/01/2022")).toBe(true);
+    expect(functions.isDate("1/1/2022")).toBe(true);
+    expect(functions.isDate("2022/11/17")).toBe(false);
+    expect(functions.isDate("11/17/22")).toBe(false);
+});
+
+test('isHexColor', () => {
+    expect(functions.isHexColor("#30D5C8")).toBe(true);
+    expect(functions.isHexColor("#FFF")).toBe(true);
+    expect(functions.isHexColor("#FFFF")).toBe(false);
+    expect(functions.isHexColor("#XYZXYZ")).toBe(false);
+});
